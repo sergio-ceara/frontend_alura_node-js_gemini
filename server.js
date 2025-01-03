7fe5dba64e93ccf21ecbaa96b34c42b13d767ed3
Desenvolvimento1 <sergio_ceara@yahoo.com.br>
2025-01-03 16:31:13 -0300

    03-01-2024 16:31

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve arquivos estáticos do diretório 'dist' gerado pelo Vite
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Para todas as outras rotas, retornar o arquivo 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
