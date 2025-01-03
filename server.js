92c6779853a02ad44e9994cf15bb86444fc1eeaa
Desenvolvimento1 <sergio_ceara@yahoo.com.br>
2025-01-03 17:57:01 -0300

    03-01-2025 17:56

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-//const express = require('express');
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 8080;
